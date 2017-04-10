e942d6bcd32fc34f7eb075535b96408439fb776e
colobot13 <colobot13@gmail.com>
2017-04-10 16:43:11 +0700

    Update main_passive.py

--- main_passive.py
+++ main_passive.py
@@ -149,14 +149,15 @@ def work_with_message(receiver):
 def queue_worker():
     global get_info_diff
     lt_info = 0
-    #print(sender.contacts_search(bot_username))
+    print(sender.contacts_search(bot_username))
     #print(sender.contacts_search(captcha_bot))
     #print(sender.contacts_search(admin_username))
     #print(sender.contacts_search(stock_bot))
     #print(sender.contacts_search(oyster_bot))
     #print(sender.contacts_search(bot_report))
-    sender.dialog_list()
     sleep(3)
+    sender.dialog_list()
+    sleep(7)
     try:
         send_msg(admin_username, "Привет Командир! Можешь управлять мной через чат. Для начала начжми команду #help")
     except Exception as err:
