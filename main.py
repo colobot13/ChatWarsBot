c3e06db68dc8fe4a53b66410096587b2268c222d
colobot13 <colobot13@gmail.com>
2017-04-21 14:20:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -180,8 +180,9 @@ def queue_worker():
     # Глобальный цикл работы программы
     while True:
         try:
-            if time() - lt_info > 300 and lt_info > 0:
+            if time() - lt_info > 1200 and lt_info > 0 and bot_enabled = True:
                 send_msg(admin_username, "Сообщения о герое давно не было. Выключаем бота")
+                log('Сообщения о герое давно не было. Выключаем бота')
                 bot_enabled = False
             if time() - lt_info > get_info_diff:
                 if arena_closed and dt.datetime.now().time() >= dt.time(13, 1) and \
